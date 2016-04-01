dad96193cabfffe8bfaed4953f7df0291ee83820
Catherine Karra <catherine.karra@gmail.com>
2016-04-01 16:40:47 -0700

    runs function for loop through divisible by 15 condition before 5 and 3, and adds keydown function to disappear previous output when new number entered in field

--- js/scripts.js
+++ js/scripts.js
@@ -3,26 +3,26 @@ function pingPong(num) {
   //for every index number from 1 to the inputted number
   for (var i = 1; i <= num; i++) {
     //if index is divisible by 3, add ping to list instead of the index
-    if (i % 3 === 0) {
-      list.push("<li>"+"ping"+"</li>");
+    if (i % 15 === 0) {
+      list.push("pingpong");
       //if index is divisible by 5, add pong to list instead of the index
     } else if (i % 5 === 0) {
-      list.push("<li>"+"pong"+"</li>");
+      list.push("pong");
       //if index is divisible by 15, add pingpong to list instead of the index
-    } else if (i % 15 === 0) {
-      list.push("<li>"+"pingpong"+"</li>");
+    } else if (i % 3 === 0) {
+      list.push("ping");
       //otherwise, add the index number to list
     } else {
-      list.push("<li>"+i+"</li>");
+      list.push(i);
     }
   }
-    return list.join(" ");
+  return list
 }
 
 //user interface logic
 $(document).ready(function() {
   $("form#input").submit(function(event) {
-    $("#result").empty();
+
     event.preventDefault();
     var numInput = $("input#num").val();
     var result = pingPong(numInput)
@@ -30,8 +30,13 @@ $(document).ready(function() {
 //make an array of numbers from 1 to the inputted number
 
 //for every index number from 0 to the end of the array of numbers
-    $("#result").append(result);
-
+  for (j = 0; j < numInput; j++) {
+    $("#result").append("<li>" + result[j] + "</li>");
+  }
     $(".output").show();
   });
+
+  $("input#num").keydown(function(){
+    $("#result").text("")
+  })
 });
