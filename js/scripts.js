b5a48e9efac7f9ab7cf68992b2f6afe58e1c31f9
Catherine Karra <catherine.karra@gmail.com>
2016-04-01 14:43:24 -0700

    fulfills requirement of populating list items on page while separating business logic and user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,22 @@
-function(num) {
+function pingPong(num) {
   var list = []
   //for every index number from 1 to the inputted number
   for (var i = 1; i <= num; i++) {
     //if index is divisible by 3, add ping to list instead of the index
     if (i % 3 === 0) {
-      list.push("ping");
+      list.push("<li>"+"ping"+"</li>");
       //if index is divisible by 5, add pong to list instead of the index
     } else if (i % 5 === 0) {
-      list.push("pong");
+      list.push("<li>"+"pong"+"</li>");
       //if index is divisible by 15, add pingpong to list instead of the index
     } else if (i % 15 === 0) {
-      list.push("pingpong");
+      list.push("<li>"+"pingpong"+"</li>");
       //otherwise, add the index number to list
     } else {
-      list.push(i);
+      list.push("<li>"+i+"</li>");
     }
   }
+    return list.join(" ");
 }
 
 //user interface logic
@@ -23,11 +24,13 @@ $(document).ready(function() {
   $("form#input").submit(function(event) {
     event.preventDefault();
     var numInput = $("input#num").val();
+    var result = pingPong(numInput)
+    // var result = pingPong(numInput);
 //make an array of numbers from 1 to the inputted number
 
 //for every index number from 0 to the end of the array of numbers
-    $("#result").append("<li>" +  + "</li>");
-    debugger
-    $("#result").show();
+    $("#result").append(result);
+
+    $(".output").show();
   });
 });
