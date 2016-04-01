3a4300d65eb988bb0916392a4088417942721cdb
Catherine Karra <catherine.karra@gmail.com>
2016-04-01 13:11:21 -0700

    separates business logic from user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,33 @@
+function(num) {
+  var list = []
+  //for every index number from 1 to the inputted number
+  for (var i = 1; i <= num; i++) {
+    //if index is divisible by 3, add ping to list instead of the index
+    if (i % 3 === 0) {
+      list.push("ping");
+      //if index is divisible by 5, add pong to list instead of the index
+    } else if (i % 5 === 0) {
+      list.push("pong");
+      //if index is divisible by 15, add pingpong to list instead of the index
+    } else if (i % 15 === 0) {
+      list.push("pingpong");
+      //otherwise, add the index number to list
+    } else {
+      list.push(i);
+    }
+  }
+}
+
 //user interface logic
 $(document).ready(function() {
   $("form#input").submit(function(event) {
     event.preventDefault();
     var numInput = $("input#num").val();
-    var list = []
+//make an array of numbers from 1 to the inputted number
 
-    //business logic
-    for (var i = 1; i <= numInput; i++) {
-      if (i % 3 === 0) {
-        list.push("ping");
-        // $("#result").append('<li>'+"ping"+'</li>');
-      } else if (i % 5 === 0) {
-        list.push("pong");
-        // $("#result").append('<li>'+"pong"+'</li>');
-      } else if (i % 15 === 0) {
-        list.push("pingpong");
-        // $("#result").append('<li>'+"pingpong"+'</li>');
-      } else {
-        list.push(i);
-        // $("#result").append('<li>'+i+'</li>');
-      }
-    }
-
-    for (var j = 0; j <= list.length; i++)
-      $("#result").text('<li>'+list(i)+'</li>')
-      debugger
-
-    $(".output").show();
+//for every index number from 0 to the end of the array of numbers
+    $("#result").append("<li>" +  + "</li>");
+    debugger
+    $("#result").show();
   });
 });
