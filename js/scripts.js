aa47179c8b8f3387cbc261a161b220391320a8b4
Catherine Karra <catherine.karra@gmail.com>
2016-04-01 09:11:59 -0700

    change append to text class in js

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,29 @@ $(document).ready(function() {
   $("form#input").submit(function(event) {
     event.preventDefault();
     var numInput = $("input#num").val();
+    var list = []
+
     //business logic
     for (var i = 1; i <= numInput; i++) {
       if (i % 3 === 0) {
-        $("#result").append('<li>'+"ping"+'</li>');
+        list.push("ping");
+        // $("#result").append('<li>'+"ping"+'</li>');
       } else if (i % 5 === 0) {
-        $("#result").append('<li>'+"pong"+'</li>');
+        list.push("pong");
+        // $("#result").append('<li>'+"pong"+'</li>');
       } else if (i % 15 === 0) {
-        $("#result").append('<li>'+"pingpong"+'</li>');
+        list.push("pingpong");
+        // $("#result").append('<li>'+"pingpong"+'</li>');
       } else {
-        $("#result").append('<li>'+i+'</li>');
+        list.push(i);
+        // $("#result").append('<li>'+i+'</li>');
       }
     }
-    $("#result").show();
+
+    for (var j = 0; j <= list.length; i++)
+      $("#result").text('<li>'+list(i)+'</li>')
+      debugger
+
+    $(".output").show();
   });
 });
